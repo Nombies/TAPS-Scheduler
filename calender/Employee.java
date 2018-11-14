2668c7744b5dfaaf2caed87a56c4db40779a5cef
zhicai4 <43509812+zhicai4@users.noreply.github.com>
2018-11-13 22:01:14 -0800

    updated constructors

--- calender/Employee.java
+++ calender/Employee.java
@@ -4,48 +4,38 @@ import java.util.*;
 
 public class Employee {
 	static List<Employee> emplist = new ArrayList<Employee>();
-	String FirstName;
-	String MiddleName;
-	String LastName;
-	String Email;
-	String Phone;
+	
 	int id;
-	boolean modify;
-	boolean att;
-	String UserName;
-	String salt;
-	String PassHash;
+	//array of taskids that this employee can do
+	List<Integer> candoTask = new ArrayList<Integer>();
+
 	Week myWeek;
 	int workhr;
-	public Employee(String FirstName,String MiddleName,String LastName,String Email,String Phone,int id, boolean modify, boolean att, String UserName,
-			 String salt, String PassHash)
+	public Employee(int id)
 	{
-		this.FirstName = FirstName;
-		this.MiddleName = MiddleName;
-		this.LastName = LastName;
-		this.Email = Email;
-		this.Phone = Phone;
+		
 		this.id = id;
-		this.modify= modify;
-		this.att = att;
-		this.UserName = UserName;
-		this.salt = salt;
-		this.PassHash = PassHash;
 		myWeek = new Week();
 		workhr = 0;
 		emplist.add(this);
+		
 	}
-	public String getFirstName(){return FirstName;}
-	public String getMiddleName(){return MiddleName;}
-	public String getLastName(){return LastName;}
-	public String getEmail(){return Email;}
-	public String getPhone(){return Phone;}
-	public String getUserName(){return FirstName;}
+	
 	public int getId(){return id;}
-	public boolean getModify(){return modify;}
-	public String getSalt(){return salt;}
-	public String getPassHash(){return PassHash;}
-	public boolean getAtt(){return att;}
+	
+	public void setCanDo(int Taskid)
+	{
+		candoTask.add(Taskid);
+	}
+	public boolean getCando(int Taskid)
+	{
+		for(int i = 0;i < candoTask.size(); i++)
+		{
+			if(candoTask.get(i) == Taskid) return true;
+		}
+		return false;
+	}
+	
 	public void setTask(Task task,int day, Time time)
 	{
 		myWeek.setTask(task, day, time);
--- calender/test.java
+++ calender/test.java
@@ -6,9 +6,9 @@ public class test {
 		// TODO Auto-generated method stub
 		boolean [] testing = new boolean [7];
 		Time xd = new Time("7:00AM");
-		Employee test = new Employee("chad","test","donkey","1@hotm.com","415-111-1234",2,true,true,"lol2","aeag","adsgadf");
-		Employee test2 = new Employee("donatello","test","donkey","1@hotm.com","415-111-1234",2,true,true,"lol2","aeag","adsgadf");
-		Task omegalul = new Task(1,"name","instr",xd,xd,1,1,testing,1,2);
+		//Employee test = new Employee("chad","test","donkey","1@hotm.com","415-111-1234",2,true,true,"lol2","aeag","adsgadf");
+		//Employee test2 = new Employee("donatello","test","donkey","1@hotm.com","415-111-1234",2,true,true,"lol2","aeag","adsgadf");
+		//Task omegalul = new Task(1,"name","instr",xd,xd,1,1,testing,1,2);
 		/*
 		System.out.println(test.getFirstName());
 		System.out.println(test.getMiddleName());
@@ -25,7 +25,7 @@ public class test {
 		//System.out.println(omegalul.getName());
 		//test.setTask(omegalul, 1, xd);
 		
-		System.out.println(test.emplist.get(1).getFirstName());
+		//System.out.println(test.emplist.get(1).getFirstName());
 	}
 
 }
